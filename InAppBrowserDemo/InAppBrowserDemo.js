1af3d7f32c2c4f66ebcc19ef2997f1147f9233ca
David Huang <3110616@qq.com>
2015-06-03 16:01:18 +0800

    update a link~~

--- InAppBrowserDemo/InAppBrowserDemo.js
+++ InAppBrowserDemo/InAppBrowserDemo.js
@@ -9,7 +9,7 @@ if (Meteor.isClient) {
           // _self: Opens in the Cordova WebView if the URL is in the white list, otherwise it opens in the InAppBrowser.
           // _blank: Opens in the InAppBrowser.
           // _system: 使用设备系统浏览器
-          var ref = window.open('http://d1.elijah.cn/donate/donate.html', '_system', 'location=yes');
+          var ref = window.open('http://donate.elijah.cn/biblevoice/biblevoice.html', '_system', 'location=yes');
       }
       
   });
